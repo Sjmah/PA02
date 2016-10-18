4ec1f76f91ae7a1d3a87fe5ca1ff963065acd882
Samuel Bernheim <sambernheim@gmail.com>
2016-10-18 13:31:51 -0400

    Remove basic stopwords and special characters from the body

--- src/code/lemma/LemmaIndexMapred.java
+++ src/code/lemma/LemmaIndexMapred.java
@@ -12,7 +12,6 @@ import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
 import code.lemma.Tokenizer;
 
 /**
- * 
  *
  */
 public class LemmaIndexMapred {
@@ -22,6 +21,33 @@ public class LemmaIndexMapred {
 		public void map(LongWritable offset, WikipediaPage page, Context context) throws IOException,
 				InterruptedException {
 			// TODO: implement Lemma Index mapper here
+
+			// gets the raw xml and stores it in a string
+			String text = page.getContent();
+			// Gets the index of the where the opening and closing body tags are
+			int openBody = text.indexOf("<body>");
+			int closeBody = text.indexOf("</body>");
+
+			//TODO: Expand this list to be an array of all stop words
+			String[] stopWords = {"and", "nor", "by", "the", "in", "around", "to"};
+			String regexStopWords = stopWords[0] + "|";
+			String regexSpecialChars = "\\,|\\-|\\||\\.|";
+
+			for (int i = 1; i < stopWords.length; i++) {
+				if (i != stopWords.length-1) {
+					regexStopWords += stopWords[i] + "|";
+				} else {
+					regexStopWords += stopWords[i];
+				}
+			}
+
+			// gets all the text in between the body tags including any tags
+			String body = text.substring(openBody+6, closeBody);
+
+			// remove all stop words and remove all special punctuation
+			body = body.replaceAll(regexStopWords, "\b");
+			body = body.replaceAll(regexSpecialChars, "");
+
 		}
 	}
 }
